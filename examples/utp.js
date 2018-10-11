c43b31bf0c7b96d7885711a6a69d4280e076edc9
Emil Bay <github@tixz.dk>
2018-10-11 21:17:52 +0200

    Add utp example

---
+++ examples/utp.js
@@ -0,0 +1,29 @@
+var utp = require('utp-native')
+var pump = require('pump')
+var peer = require('..')
+
+var server = utp.createServer(function onconnection (rawStream) {
+  var sec = peer(rawStream, false)
+
+  pump(sec, sec, function (err) {
+    if (err) throw err
+  })
+})
+
+server.listen(function () {
+  var port = server.address().port
+
+  var clientRawStream = utp.connect(port)
+  var clientSec = peer(clientRawStream, true)
+
+  clientSec.on('data', function (data) {
+    console.log(data.toString())
+  })
+
+  clientSec.on('end', function () {
+    server.close()
+  })
+
+  clientSec.write('Hello world')
+  clientSec.end()
+})
