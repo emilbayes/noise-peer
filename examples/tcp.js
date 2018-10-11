6a003e5b407fc7fbcf1f0d65a9cd8e616a7017dd
Emil Bay <github@tixz.dk>
2018-10-11 20:44:41 +0200

    Move example to tcp.js

---
+++ examples/tcp.js
@@ -0,0 +1,32 @@
+var net = require('net')
+var pump = require('pump')
+var peer = require('..')
+
+var server = net.createServer(function onconnection (rawStream) {
+  var sec = peer(rawStream, false)
+
+  pump(sec, sec, function (err) {
+    if (err) return console.error('server', err)
+    console.log('server eos')
+  })
+})
+
+server.listen(function () {
+  var port = server.address().port
+
+  var clientRawStream = net.connect(port)
+  var clientSec = peer(clientRawStream, true)
+
+  clientSec.on('data', function (data) {
+    console.log(data.toString())
+  })
+
+  clientSec.on('end', function () {
+    server.close()
+    clientRawStream.end()
+    console.log('client eos')
+  })
+
+  clientSec.write('Hello world')
+  clientSec.end()
+})
