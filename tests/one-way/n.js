fb0e13f0d2eaca47196de9e1cdbf8c2b5a907837
Emil Bay <github@tixz.dk>
2018-10-15 10:31:15 +0200

    Simple one-way stream

---
+++ tests/one-way/n.js
@@ -0,0 +1,32 @@
+var test = require('tape')
+var peer = require('../..')
+var transport = require('../helpers/raw-stream')
+
+// this test is still work in progress
+test('N server write', function (assert) {
+  assert.plan(1)
+  var t = transport()
+
+  var serverKeys = peer.keygen()
+  var client = peer(t.a, true, {
+    pattern: 'N',
+    remoteStaticKey: serverKeys.publicKey
+  })
+  var server = peer(t.b, false, {
+    pattern: 'N',
+    staticKeyPair: serverKeys
+  })
+
+  server.write('Hello World')
+  server.end()
+
+  var body = ''
+  client.on('data', function (ch) {
+    body += ch
+  })
+
+  client.on('end', function () {
+    assert.equal(body, 'Hello World', 'Should be same string')
+    assert.end()
+  })
+})
